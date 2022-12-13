1d94688819f9237f3c90166d3a9e3a5962c993c8
edwces <68016750+EdwinC0@users.noreply.github.com>
2022-12-13 12:32:54 +0100

    more members tests

--- test/employee_rewards/members_test.exs
+++ test/employee_rewards/members_test.exs
@@ -1,6 +1,8 @@
 defmodule EmployeeRewards.MembersTest do
   use EmployeeRewards.DataCase
 
+  alias EmployeeRewards.RewardsFixtures
+  alias EmployeeRewards.Rewards
   alias EmployeeRewards.Members
 
   describe "members" do
@@ -10,6 +12,8 @@ defmodule EmployeeRewards.MembersTest do
 
     @invalid_attrs %{}
     @points_update_attrs %{points: 20}
+    @pool_update_attrs %{pool: 20}
+    @transfer_points_attrs %{points: 5}
 
     test "list_members/0 returns all members" do
       member = member_fixture()
@@ -42,9 +46,18 @@ defmodule EmployeeRewards.MembersTest do
       assert %Ecto.Changeset{} = Members.change_member(member)
     end
 
-    test "change_member_points/2 returns a member changeset" do
+    test "change_member_points/2 returns a valid member changeset" do
       member = member_fixture()
-      assert %Ecto.Changeset{} = Members.change_member_points(member, @points_update_attrs)
+      changeset = Members.change_member_points(member, @points_update_attrs)
+      assert %Ecto.Changeset{} = changeset
+      assert changeset.valid?
+    end
+
+    test "change_member_pool/2 returns a valid member changeset" do
+      member = member_fixture()
+      changeset = Members.change_member_pool(member, @pool_update_attrs)
+      assert %Ecto.Changeset{} = changeset
+      assert changeset.valid?
     end
 
     test "get_member_by_credentials/1 with valid data returns a member with given credentials" do
@@ -57,10 +70,54 @@ defmodule EmployeeRewards.MembersTest do
       assert {1, nil} = Members.reset_members_points()
     end
 
-    test "reset_members_points/0 sets all member points to 50" do
+    test "reset_members_points/0 sets all member pools to 50" do
       member = member_fixture()
       Members.reset_members_points()
-      assert Members.get_member!(member.id).points == 50
+      assert Members.get_member!(member.id).pool == 50
+    end
+
+    test "transfer_member_points/3 with valid data perform a points transfer" do
+      member_from = member_fixture(@pool_update_attrs)
+      member_to = member_fixture()
+
+      assert {:ok, %{member_to: to, member_from: from}} =
+               Members.transfer_member_points(member_from, member_to, @transfer_points_attrs)
+
+      assert from.pool == member_from.pool - @transfer_points_attrs.points
+      assert to.points == member_to.points + @transfer_points_attrs.points
+    end
+
+    test "transfer_member_points/3 creates Reward entry" do
+      member_from = member_fixture(@pool_update_attrs)
+      member_to = member_fixture()
+
+      assert {:ok, %{create_reward_entry: reward}} =
+               Members.transfer_member_points(member_from, member_to, @transfer_points_attrs)
+
+      assert %Rewards.Reward{} = reward
+    end
+
+    test "list_members_with_rewards/0 returns all members with their associated reward entries" do
+      member = member_fixture()
+      reward = RewardsFixtures.reward_fixture(%{member: member})
+      result = Members.list_members_with_rewards()
+
+      assert Enum.map(result, fn member -> member.id end) == [
+               member.id
+             ]
+
+      assert Enum.map(result, fn member ->
+               Enum.map(member.rewards, fn reward -> reward.id end)
+             end) == [
+               [reward.id]
+             ]
+    end
+
+    test "get_member_rewards_by_id/1 returns rewards associated with given member" do
+      member = member_fixture()
+      reward = RewardsFixtures.reward_fixture(%{member: member})
+      result = Members.get_member_rewards_by_id(member.id)
+      Enum.map(result, fn reward -> reward.id end) == [reward.id]
     end
   end
 end
